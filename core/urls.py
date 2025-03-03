33eaaae19c6f377bc0ad0943d2659625878b7c91
LikeWise12345 <Touqeerwazeer123@gmail.com>
2025-03-03 13:08:55 +0000

    first commit

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,13 @@ urlpatterns = [
     path('signup/', UserSignupView.as_view(), name='signup'),
     path('login/', UserLoginView.as_view(), name='login'),
     path("csrf/", csrf_token_view, name="csrf_token"),  # CSRF token endpoint
+    
 ]
+
+# VideoListView.as_view(), name='video-list'),
+#     path('videos/upload/', UploadVideoView.as_view(), name='video-upload'),
+#     path('videos/<int:video_id>/comment/', add_comment, name='add-comment'),
+#     path('videos/<int:video_id>/rate/', rate_video, name='rate-video'),
+#     path('signup/', UserSignupView.as_view(), name='signup'),
+#     path('login/', UserLoginView.as_view(), name='login'),
+#     path("csrf/", csrf_token_view, name="csrf_token"),  
